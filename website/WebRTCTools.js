f1f0699c1764901a3abfffb68f867390b6e3a554
Michae Hart <hartmj99@cs.washington.edu>
2019-05-17 05:13:08 -0700

    Expand WebRTCTools usability

--- website/WebRTCTools.js
+++ website/WebRTCTools.js
@@ -1,4 +1,32 @@
 class WebRTCTools {
+  static createPeerConn(SM, to) {
+    let peerConn = new RTCPeerConnection();
+
+    peerConn.onicecandidate = (e) => {
+      if (!peerConn || !e || !e.candidate)
+        return;
+      var candidate = e.candidate;
+      SM.sendSignal(to, "candidate", candidate);
+      console.log("ICE sent");
+    };
+
+    return peerConn;
+  }
+
+  static async startStream(peerConn, streamID) {
+    const video = document.getElementById(streamID);
+    const stream = await navigator.mediaDevices.getUserMedia(
+                            {video: {
+                               facingMode: 'environment',
+                               frameRate: {ideal: 20, max: 30}
+                             }, 
+                             audio: false});
+
+    video.srcObject = stream;
+    video.hidden = false;
+    peerConn.addTrack(stream.getVideoTracks()[0], stream);
+  }
+
   static receiveCandidate(peerConn, json) {
     try {
       peerConn.addIceCandidate(new RTCIceCandidate(json.data));
