f9dee2b6b112539c12ce792726a78e0a3244e64c
Michae Hart <hartmj99@cs.washington.edu>
2019-05-17 00:53:31 -0700

    Refactor projector to use ServerManager

--- website/projector/script.js
+++ website/projector/script.js
@@ -1,98 +1,62 @@
 'use strict';
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+import ServerManager from "../ServerManager.js";
 
 /////////////////////////
 //  SIGNALLING SERVER  //
 /////////////////////////
 
-const HOST = "www.thejobdance.com";
-const PORT = 789;
+const SM = new ServerManager("projector");
 
-const CONN = new WebSocket("wss://" + HOST + ":" + PORT, 'request-projector');
-
-const my_id = "projector";
-
-CONN.onerror = 
-  (err) => console.log('server\'s error: ' + err);
-
-async function sendSignal(to, action, data) {
-  let count = 0;
-  while (CONN.readyState === 0 && count < 400) {
-    await sleep(5);
-    count++;
-  }
-  if (CONN.readyState === 1) {
-    let json = {
-      from: my_id,
-      to: to,
-      action: action,
-      data: data
-    };
-    CONN.send(JSON.stringify(json));
-  }
-}
-
-CONN.onmessage = function(e){
-  console.log(e.data);
-  var json = JSON.parse(e.data);
-
-  if(json.action === "candidate"){
-    processCandidate(json.from, json.data);
-  } else if(json.action === "offer") {
-    processOffer(json.from, json.data)
-  } else if(json.action === "disconnect") {
-    disconnectStream();
-  } else {
-    console.log("unexpected action: " + json.action);
-  }
-}
+SM.addHandler( "candidate", processCandidate);
+SM.addHandler(     "offer", processOffer);
+SM.addHandler("disconnect", processDisconnect);
 
 //////////////
 //  WebRTC  //
 //////////////
 
-var peerConnection = new RTCPeerConnection();
+var pc;
 
 function openPeerConn() {
-  peerConnection.onicecandidate = (e) => {
-    if (!peerConnection || !e || !e.candidate)
+  let peerConn = new RTCPeerConnection();
+
+  peerConn.onicecandidate = (e) => {
+    if (!peerConn || !e || !e.candidate)
       return;
     var candidate = e.candidate;
-    sendSignal('controller', "candidate", candidate);
+    SM.sendSignal('controller', "candidate", candidate);
     console.log("ICE sent");
   };
 
   const video = document.querySelector('#vid');
-  peerConnection.ontrack = (e) => {
+  peerConn.ontrack = (e) => {
     console.log("stream received");
-    if (video.srcObject !== e.streams[0]) {
-      video.srcObject = e.streams[0];
-    }
+    video.srcObject = e.streams[0];
   }
-  video.hidden = false;
+
+  return peerConn;
 }
 
-function processCandidate(from, iceCandidate){
+function processCandidate(json){
   try {
-    peerConnection.addIceCandidate(new RTCIceCandidate(iceCandidate));
+    pc.addIceCandidate(new RTCIceCandidate(json.data));
   } catch (e) {}
 }
 
-function processOffer(stream_id, offer){
-  openPeerConn(0);
-  peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+function processOffer(json){
+  pc = openPeerConn();
+
+  pc.setRemoteDescription(new RTCSessionDescription(json.data));
   var sdpConstraints = {
     'mandatory': {
       'OfferToReceiveAudio': false,
       'OfferToReceiveVideo': true
     }
   };
-  peerConnection.createAnswer(sdpConstraints).then(sdp => {
-    peerConnection.setLocalDescription(sdp).then(() => {           
-      sendSignal('controller', "answer", sdp);
+  pc.createAnswer(sdpConstraints).then(sdp => {
+    pc.setLocalDescription(sdp).then(() => {           
+      SM.sendSignal('controller', "answer", sdp);
       console.log("answer sent");
     })
   }, function(err) {
@@ -100,9 +64,11 @@ function processOffer(stream_id, offer){
   });
 };
 
-function disconnectStream() {
+function processDisconnect() {
   const video = document.querySelector('#vid');
-  video.srcObject.getTracks()[0].stop();
-  video.hidden = true;
+  if (video.srcObject !== null) {
+    video.srcObject.getTracks()[0].stop();
+    video.srcObject = null;
+  }
 }
 
