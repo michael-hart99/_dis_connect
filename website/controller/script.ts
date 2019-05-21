9b1718d75058ea72b9b74d72fd62b2b6e524d321
Michae Hart <hartmj99@cs.washington.edu>
2019-05-20 18:55:18 -0700

    Restructure controller and add Javadoc

--- website/controller/script.ts
+++ website/controller/script.ts
@@ -1,20 +1,18 @@
 'use strict';
 
-import { ServerManager, ServerMessage } from '../ServerManager.ts';
-import WebRTCTools from '../WebRTCTools.ts';
+import { ServerMessage } from '../../server/ServerInfo';
+import { ServerManager } from '../ServerManager';
+import { WebRTCTools } from '../WebRTCTools';
 
-/////////////////////////
-//  SIGNALLING SERVER  //
-/////////////////////////
-
+// The server connection that will send/receive messages
 const SM = new ServerManager('controller');
 
-//////////////
-//  WebRTC  //
-//////////////
-
+// The RTC connection with the projector
 var pc: RTCPeerConnection;
 
+/**
+ * Initializes a PeerConnection and opens the camera's video stream.
+ */
 async function initConn(): Promise<void> {
   pc = WebRTCTools.createPeerConn(SM, 'projector');
 
@@ -23,66 +21,93 @@ async function initConn(): Promise<void> {
   WebRTCTools.sendOffer(SM, pc, 'projector');
 }
 
+/**
+ * Updates the RTC connection with the given ICE candidate information.
+ *
+ * @param {ServerMessage} json A message with ICE candidate information.
+ */
 function processCandidate(json: ServerMessage): void {
   WebRTCTools.receiveCandidate(pc, json);
 }
 
+/**
+ * Updates the RTC connection with the given offer details.
+ *
+ * @param {ServerMessage} json A message with peer session description information.
+ */
 function processAnswer(json: ServerMessage): void {
   WebRTCTools.receiveAnswer(pc, json);
 }
 
-///////////////////////////////
-
+/**
+ * Drops the connection with the projector and updates the webpage visually to
+ *     reflect that.
+ */
 function processDisconnect(): void {
-  const video = document.getElementById('vid');
-  if (
-    video instanceof HTMLVideoElement &&
-    video.srcObject instanceof MediaStream
-  ) {
-    video.hidden = true;
-    video.srcObject.getTracks()[0].stop();
-    SM.sendSignal('projector', 'disconnect', null);
-  }
+  const video = document.getElementById('vid') as HTMLVideoElement;
+  const src = video.srcObject as MediaStream;
+
+  video.hidden = true;
+  src.getTracks()[0].stop();
+  SM.sendSignal('projector', 'disconnect', null);
 }
 
+/**
+ * Send message to begin the embedded video.
+ */
 function beginVid(): void {
-  SM.sendSignal('all', 'begin_video', null);
+  SM.sendSignal('all', 'beginVideo', null);
 }
 
+/**
+ * Send message to restart the embedded video.
+ */
 function resetVid(): void {
-  SM.sendSignal('all', 'reset_video', null);
+  SM.sendSignal('all', 'resetVideo', null);
 }
 
+/**
+ * Send message to blackout the webpage.
+ */
 function blackout(): void {
   SM.sendSignal('all', 'blackout', null);
 }
 
-function multiStream(): void {
-  SM.sendSignal('all', 'can_stream', null);
+/**
+ * Send message to enable streaming.
+ */
+function canStream(): void {
+  SM.sendSignal('all', 'canStream', null);
 }
 
-/////////////////
+////////////////////////////
 
+// Add action handlers to run when messages are received
 SM.addHandler('candidate', processCandidate);
 SM.addHandler('answer', processAnswer);
 
-//////////////////////////
-
-(document
-  .querySelector('#run') as HTMLButtonElement)
-  .addEventListener('click', initConn);
-(document
-  .querySelector('#disconnect') as HTMLButtonElement)
-  .addEventListener('click', processDisconnect);
-(document
-  .querySelector('#begin_vid') as HTMLButtonElement)
-  .addEventListener('click', beginVid);
-(document
-  .querySelector('#reset_vid') as HTMLButtonElement)
-  .addEventListener('click', resetVid);
-(document
-  .querySelector('#blackout') as HTMLButtonElement)
-  .addEventListener('click', blackout);
-(document
-  .querySelector('#multi_stream') as HTMLButtonElement)
-  .addEventListener('click', multiStream);
+// Add event to the HTML buttons
+(document.querySelector('#run') as HTMLButtonElement).addEventListener(
+  'click',
+  initConn
+);
+(document.querySelector('#disconnect') as HTMLButtonElement).addEventListener(
+  'click',
+  processDisconnect
+);
+(document.querySelector('#begin_vid') as HTMLButtonElement).addEventListener(
+  'click',
+  beginVid
+);
+(document.querySelector('#reset_vid') as HTMLButtonElement).addEventListener(
+  'click',
+  resetVid
+);
+(document.querySelector('#blackout') as HTMLButtonElement).addEventListener(
+  'click',
+  blackout
+);
+(document.querySelector('#multi_stream') as HTMLButtonElement).addEventListener(
+  'click',
+  canStream
+);
