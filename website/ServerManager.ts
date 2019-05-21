7cf45b171443d2d365340b0b9c5aa9acc268abc7
Michae Hart <hartmj99@cs.washington.edu>
2019-05-20 18:52:16 -0700

    Update ServerManager to cache unsent messages and add Javadoc

--- website/ServerManager.ts
+++ website/ServerManager.ts
@@ -1,44 +1,76 @@
 'use strict';
 
-const HOST = 'www.thejobdance.com';
-const PORT = 789;
+import { HOST, PORT, ServerMessage, PacketData } from '../server/ServerInfo';
 
-export interface ServerMessage {
-  from: string;
+/**
+ * Returns a promise that resolves in a given number of milliseconds.
+ *
+ * @param {number} ms The number of milliseconds before the returned promise
+ *      resolves.
+ */
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve: Function): number => setTimeout(resolve, ms));
+}
+
+/**
+ * PartialMessage represents a message that doesn't have "from" information.
+ *     This is likely because the connection hasn't transmitted it's ID.
+ */
+interface PartialMessage {
   to: string;
   action: string;
-  data: any;
-};
+  data: PacketData;
+}
 
+/**
+ * ServerManager is a wrapper around a Websocket connection that will format
+ *     messages to be readable by the signalling server. ServerManager also
+ *     will interpret messages and send responses to proper handler functions.
+ *
+ * The server this is connected to is known as a signalling server and will
+ *     route messages to addressed connections.
+ */
 export class ServerManager {
-  /* TODO */
-  private _id: string;
+  /** The ID of this server connection. This is supplied by the server. */
+  private _id: string | null;
 
-  /* TODO */
+  /** If true, this connection is ready to send and receive messages. */
+  private _ready: boolean;
+
+  /** A mapping from action names to handler functions used to route signals
+      to functions that will handle the response. */
   private _handlers: Map<string, Function>;
 
-  /* TODO */
+  /** The signalling server connection. */
   private _connection: WebSocket;
 
+  /** Messages sent while the server connection was not ready. */
+  private _unsentMessages: PartialMessage[];
+
   /**
-   * TODO
+   * Instantiates the signalling server connection.
+   *
+   * @param {string} requestLabel The identifying label of this request.
    */
-  public constructor(protocolID: string) {
-    this._id = protocolID;
+  public constructor(requestLabel: string) {
+    this._id = null;
+
+    this._ready = false;
 
     this._handlers = new Map();
 
     this._connection = new WebSocket(
       'wss://' + HOST + ':' + PORT,
-      'request-' + protocolID
+      'request-' + requestLabel
     );
 
     this._connection.onerror = (): void =>
       console.log('Error connecting to server');
 
+    // Set onmessage to route the message through a handler function
     let thisObj = this;
     this._connection.onmessage = (e: MessageEvent): void => {
-      let json = JSON.parse(e.data);
+      let json: ServerMessage = JSON.parse(e.data);
       console.log('received %s from %s', json.action, json.from);
 
       let handler = thisObj._handlers.get(json.action);
@@ -48,50 +80,80 @@ export class ServerManager {
         console.log('Unexpected action received: %s', json.action);
       }
     };
+
+    this._unsentMessages = [];
+
+    // Add a handler to handle the initial data sent by the server.
+    this.addHandler(
+      '_initialize',
+      (json: ServerMessage): void => {
+        this._id = json.data as string;
+        this._ready = true;
+        this._handlers.delete('_initialize');
+
+        let message: PartialMessage | undefined;
+        while ((message = this._unsentMessages.pop())) {
+          this.sendSignal(message.to, message.action, message.data);
+        }
+      }
+    );
   }
 
   /**
-   * TODO
+   * Returns if the server is ready to transmit messages. Note that messages
+   *     can still be received even if they can't be transmitted.
+   *
+   * @returns {boolean} True if the server is ready to send messages.
    */
-  public static sleep(ms: number): Promise<void> {
-    return new Promise((resolve: Function): number => setTimeout(resolve, ms));
+  public isReady(): boolean {
+    return this._ready;
   }
 
   /**
-   * TODO
-   */
-  public setID(id: string): void {
-    this._id = id;
-  }
-
-  /**
-   * TODO
+   * Add a handler function to a specified action. Any incoming messages
+   *     requesting this action will be routed to the given function.
+   *
+   * @param {string}   action The action the given function should handle.
+   * @param {Function} fn     The function that will handle the message.
    */
   public addHandler(action: string, fn: Function): void {
     this._handlers.set(action, fn);
   }
 
   /**
-   * TODO
+   * Send a message requesting action to a specified recipient.
+   *
+   * @param {string}     to     The addressee of the message.
+   * @param {string}     action The type of action to take.
+   * @param {PacketData} data   Any data needed to perform the action.
    */
   public async sendSignal(
     to: string,
     action: string,
-    data: any
+    data: PacketData
   ): Promise<void> {
-    let count = 0;
-    while (this._connection.readyState === 0 && count < 400) {
-      await ServerManager.sleep(5);
-      count++;
-    }
-    if (this._connection.readyState === 1) {
-      let json = {
-        from: this._id,
+    if (this._ready) {
+      let count = 0;
+      while (this._connection.readyState === 0 && count < 400) {
+        await sleep(5);
+        count++;
+      }
+      if (this._connection.readyState === 1 && this._id) {
+        let message: ServerMessage = {
+          from: this._id,
+          to: to,
+          action: action,
+          data: data,
+        };
+        this._connection.send(JSON.stringify(message));
+      }
+    } else {
+      let message: PartialMessage = {
         to: to,
         action: action,
         data: data,
       };
-      this._connection.send(JSON.stringify(json));
+      this._unsentMessages.push(message);
     }
   }
 }
