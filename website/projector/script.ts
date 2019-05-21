77372d10ee932e841e4faab5eed24e4f686dd95f
Michae Hart <hartmj99@cs.washington.edu>
2019-05-20 18:54:14 -0700

    Restructure projector and add Javadoc

--- website/projector/script.ts
+++ website/projector/script.ts
@@ -1,56 +1,71 @@
 'use strict';
 
-import { ServerManager, ServerMessage } from '../ServerManager.ts';
-import WebRTCTools from '../WebRTCTools.ts';
+import { ServerMessage } from '../../server/ServerInfo';
+import { ServerManager } from '../ServerManager';
+import { WebRTCTools } from '../WebRTCTools';
 
-/////////////////////////
-//  SIGNALLING SERVER  //
-/////////////////////////
+// The server connection that will send/receive messages
+const SM = new ServerManager('projector');
 
-const SM = new ServerManager("projector");
-
-SM.addHandler( "candidate", processCandidate);
-SM.addHandler(     "offer", processOffer);
-SM.addHandler("disconnect", processDisconnect);
-
-//////////////
-//  WebRTC  //
-//////////////
-
+// The RTC connection with the controller
 var pc: RTCPeerConnection;
 
+/**
+ * Initializes a PeerConnection.
+ *
+ * @returns {RTCPeerConnection} A new connection with the controller.
+ */
 function initConn(): RTCPeerConnection {
-  let peerConn = WebRTCTools.createPeerConn(SM, "controller");
+  let peerConn = WebRTCTools.createPeerConn(SM, 'controller');
 
-  const video = document.querySelector('#vid');
-  if (video instanceof HTMLVideoElement) {
-    console.log(peerConn);
-    peerConn.ontrack = (e: RTCTrackEvent) => {
-      console.log("stream received");
-      video.srcObject = e.streams[0];
-    }
-  }
+  const video = document.querySelector('#vid') as HTMLVideoElement;
+  peerConn.ontrack = (e: RTCTrackEvent): void => {
+    console.log('stream received');
+    video.srcObject = e.streams[0];
+  };
 
   return peerConn;
 }
 
-function processOffer(json: ServerMessage): void {
-  pc = initConn();
-
-  WebRTCTools.receiveOffer(SM, pc, json)
-}
-
+/**
+ * Updates the RTC connection with the given ICE candidate information.
+ *
+ * @param {ServerMessage} json A message with ICE candidate information.
+ */
 function processCandidate(json: ServerMessage): void {
   WebRTCTools.receiveCandidate(pc, json);
 }
 
+/**
+ * Updates the RTC connection with the given offer details.
+ *
+ * @param {ServerMessage} json A message with peer session description information.
+ */
+function processOffer(json: ServerMessage): void {
+  pc = initConn();
+
+  WebRTCTools.receiveOffer(SM, pc, json);
+}
+
 ////////////////////////
 
+/**
+ * Drops the connection with the controller and updates the webpage visually to
+ *     reflect that.
+ */
 function processDisconnect(): void {
-  const video = document.querySelector('#vid');
-  if (video instanceof HTMLVideoElement &&
-      video.srcObject instanceof MediaStream) {
-    video.srcObject.getTracks()[0].stop();
+  const video = document.querySelector('#vid') as HTMLVideoElement;
+  const src = video.srcObject as MediaStream | null;
+
+  if (src !== null) {
+    src.getTracks()[0].stop();
     video.srcObject = null;
   }
 }
+
+/////////////////////////
+
+// Add action handlers to run when messages are received
+SM.addHandler('candidate', processCandidate);
+SM.addHandler('offer', processOffer);
+SM.addHandler('disconnect', processDisconnect);
