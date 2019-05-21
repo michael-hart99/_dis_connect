5e2b0b967fc1cb5cdca35cf4135f24b3b0147c24
Michae Hart <hartmj99@cs.washington.edu>
2019-05-20 18:55:37 -0700

    Restructure streamHost and add Javadoc

--- website/streamHost/script.ts
+++ website/streamHost/script.ts
@@ -1,52 +1,150 @@
 'use strict';
 
-import { ServerManager, ServerMessage } from '../ServerManager.ts';
-import WebRTCTools from '../WebRTCTools.ts';
+import { ServerMessage } from '../../server/ServerInfo';
+import { ServerManager } from '../ServerManager';
+import { WebRTCTools } from '../WebRTCTools';
 
-/////////////////////////
-//  SIGNALLING SERVER  //
-/////////////////////////
-
-const SM = new ServerManager("streamHost");
-
-SM.addHandler( "candidate", processCandidate);
-SM.addHandler(     "offer", processOffer);
-SM.addHandler("disconnect", processDisconnect);
-
-//////////////
-//  WebRTC  //
-//////////////
-
+/**
+ * Pairs a Peer Connection with the camera feed that will be streamed.
+ *     If this stream is not currently being displayed, inUseIndex == -1.
+ */
 interface StreamConn {
   peerConn: RTCPeerConnection;
   stream: MediaStream;
-};
+  inUseIndex: number;
+}
 
-// TODO
+// The server connection that will send/receive messages
+const SM = new ServerManager('streamHost');
+
+// Maps unique connection IDs to RTC data and the video streams
 var conns: Map<number, StreamConn> = new Map();
 
-// TODO
-var inUse = [-1, -1, -1,
-             -1, -1, -1,
-             -1, -1, -1];
+// Represents the current state of the webpage's grid of 9 videos.
+//     Values >=0 represent video squares that are in-use.
+var inUse = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+console.log(inUse);
+
+// The minimum delay between flickers (in seconds)
+const MIN_FLICKER_DELAY = 5;
+// The maximum delay between flickers (in seconds)
+const MAX_FLICKER_DELAY = 15;
 
 /**
- * TODO
+ * Choses random streams to replace the currently visible live feeds. This
+ *     algorithm will choose first from any stream and place it in a random
+ *     video element. Then, if that stream had already been visible, the
+ *     previous location of the stream will be overwritten with a stream that
+ *     was not on-screen.
+ *
+ * This choice was made to stop the viewer from only
+ *     watching one stream since they're constantly in-motion, and also to
+ *     make streams that aren't visible likely to become visible.
+ *
+ * This function will call itself again after a few seconds.
+ */
+function flicker(): void {
+  if (conns.size > 9) {
+    let streamKeys = Array.from(conns.keys());
+
+    // Chose a random stream and location to place it
+    let chosenKey = Math.floor(Math.random() * conns.size);
+    let chosenStreamID = streamKeys[chosenKey];
+    let chosenConn = conns.get(chosenStreamID);
+    let chosenVideo = Math.floor(Math.random() * 9);
+
+    if (chosenConn) {
+      // The ID of the stream being replaced
+      let prevStreamID = inUse[chosenConn.inUseIndex];
+
+      // If this stream was already on-screen, choose one from off-screen to
+      // take its previous spot
+      if (inUse.includes(chosenStreamID)) {
+        let notInUse = streamKeys.filter((x): boolean => !inUse.includes(x));
+
+        let chosenUnusedKey = Math.floor(Math.random() * notInUse.length);
+        let chosenUnusedStreamID = notInUse[chosenUnusedKey];
+        let chosenUnusedConn = conns.get(chosenUnusedStreamID);
+
+        if (chosenUnusedConn) {
+          let video = document.querySelector(
+            '#stream' + (chosenConn.inUseIndex + 1)
+          ) as HTMLVideoElement;
+
+          video.srcObject = chosenUnusedConn.stream;
+          inUse[chosenConn.inUseIndex] = chosenUnusedStreamID;
+          chosenUnusedConn.inUseIndex = chosenConn.inUseIndex;
+        }
+      }
+
+      // Place the chosen stream in the chosen position
+      let video = document.querySelector(
+        '#stream' + (chosenVideo + 1)
+      ) as HTMLVideoElement;
+      video.srcObject = chosenConn.stream;
+      inUse[chosenVideo] = chosenStreamID;
+      chosenConn.inUseIndex = chosenVideo;
+
+      if (prevStreamID !== -1) {
+        let prevConn = conns.get(prevStreamID);
+        if (prevConn) {
+          prevConn.inUseIndex = -1;
+        }
+      }
+
+      console.log('flickered');
+    }
+  }
+
+  setTimeout(
+    flicker,
+    Math.random() * ((MAX_FLICKER_DELAY - MIN_FLICKER_DELAY) * 1000) +
+      MIN_FLICKER_DELAY * 1000
+  );
+}
+
+/**
+ * Initializes a PeerConnection and prepares to receive media.
+ *
+ * @param {string} from The ID of the peer to connect with.
+ *
+ * @returns {RTCPeerConnection} A new connection with the specified peer.
  */
 function initConn(from: string): RTCPeerConnection {
   let peerConn = WebRTCTools.createPeerConn(SM, from);
-  
-  let video = document.querySelector('#stream' + (Number(from) % 9 + 1)) as HTMLVideoElement;
-  peerConn.ontrack = (e: RTCTrackEvent) => {
-    console.log("stream received");
+  let fromAsNum = Number(from);
+
+  let video = document.querySelector(
+    '#stream' + ((fromAsNum % 9) + 1)
+  ) as HTMLVideoElement;
+  // Update video element with live stream once connection is established
+  peerConn.ontrack = (e: RTCTrackEvent): void => {
+    console.log('stream received');
+    let useIndex = fromAsNum % 9;
     video.srcObject = e.streams[0];
-    conns.set(Number(from), {peerConn: peerConn, stream: e.streams[0]});
-    inUse[Number(from) % 9] = Number(from);
-  }
+    conns.set(fromAsNum, {
+      peerConn: peerConn,
+      stream: e.streams[0],
+      inUseIndex: useIndex,
+    });
+    if (inUse[useIndex] !== -1) {
+      let prevStream = conns.get(inUse[useIndex]);
+      if (prevStream) {
+        prevStream.inUseIndex = -1;
+      }
+    }
+    inUse[useIndex] = fromAsNum;
+  };
 
   return peerConn;
 }
 
+/**
+ * Updates the corresponding peer connection with the given ICE candidate
+ *     information.
+ *
+ * @param {ServerMessage} json A message with ICE candidate information.
+ */
 function processCandidate(json: ServerMessage): void {
   let conn = conns.get(Number(json.from));
   if (conn) {
@@ -55,7 +153,9 @@ function processCandidate(json: ServerMessage): void {
 }
 
 /**
- * TODO
+ * Updates the corresponding peer connection with the given offer details.
+ *
+ * @param {ServerMessage} json A message with peer session description information.
  */
 function processOffer(json: ServerMessage): void {
   let peerConn = initConn(json.from);
@@ -63,72 +163,34 @@ function processOffer(json: ServerMessage): void {
   WebRTCTools.receiveOffer(SM, peerConn, json);
 }
 
-////////////////////////
-//  MANAGING STREAMS  //
-////////////////////////
-
-// The minimum delay between flickers (in seconds)
-const MIN_FLICKER_DELAY = 5;
-// The maximum delay between flickers (in seconds)
-const MAX_FLICKER_DELAY = 15;
-
 /**
- * TODO
+ * Drops the given connection and updates the webpage visually to reflect
+ *     that.
+ *
+ * @param {ServerMessage} json A message detailing who disconnected.
  */
-async function flicker(): Promise<void> {
-  if (conns.size > 9) {
-    let stream_keys = Array.from(conns.keys());
-
-    let chosen_key = Math.floor(Math.random() * conns.size);
-    let chosen_stream = stream_keys[chosen_key];
-    let chosen_video = Math.floor(Math.random() * 9);
-
-    if (inUse.includes(chosen_stream)) {
-      let notInUse = stream_keys.filter(x => !inUse.includes(x));
-
-      let chosen_unused_key = Math.floor(Math.random() * notInUse.length);
-      let chosen_unused_stream = notInUse[chosen_unused_key];
-
-      for (let i = 0; i < inUse.length; ++i) {
-        if (inUse[i] === chosen_stream) {
-          inUse[i] = chosen_unused_stream;
-          let video = document.querySelector('#stream' + (i + 1)) as HTMLVideoElement;
-          let stream = conns.get(chosen_unused_stream);
-          if (stream) {
-            video.srcObject = stream.stream;
-          }
-        }
-      }
+function processDisconnect(json: ServerMessage): void {
+  const thisConn = conns.get(Number(json.from));
+  if (thisConn) {
+    const video = document.querySelector(
+      '#stream' + (thisConn.inUseIndex + 1)
+    ) as HTMLVideoElement;
+    const src = video.srcObject as MediaStream | null;
+    if (src !== null) {
+      src.getTracks()[0].stop();
+      video.srcObject = null;
+      inUse[thisConn.inUseIndex] = -1;
     }
-
-    inUse[chosen_video] = chosen_stream;
-    let video = document.querySelector('#stream' + (chosen_video + 1)) as HTMLVideoElement;
-    let stream = conns.get(chosen_stream);
-    if (stream) {
-      video.srcObject = stream.stream;
-    }
-
-    console.log("flickered");
-  }
-  await ServerManager.sleep(Math.random() * ((MAX_FLICKER_DELAY - MIN_FLICKER_DELAY) * 1000)
-              + (MIN_FLICKER_DELAY * 1000));
-  flicker();
-}
-
-/**
- * TODO
- */
-function processDisconnect(json: ServerMessage) {
-  try {
     conns.delete(Number(json.from));
-    for (let i = 0; i < inUse.length; ++i) {
-      if (inUse[i] === Number(json.from)) {
-        inUse[i] = -1;
-        let video = document.querySelector('#stream' + (i + 1)) as HTMLVideoElement;
-        video.srcObject = null;
-      }
-    }
-  } catch (e) {}
+  }
 }
 
+///////////////
+
+// Add action handlers to run when messages are received
+SM.addHandler('candidate', processCandidate);
+SM.addHandler('offer', processOffer);
+SM.addHandler('disconnect', processDisconnect);
+
+// Start flickering the live feeds
 flicker();
