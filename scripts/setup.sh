3a68804783f3f63959229537cbeb38c2cb7d2a35
Michae Hart <hartmj99@cs.washington.edu>
2019-05-22 03:49:36 -0700

    Add new setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tput setaf 14;
+printf "\n Creating website directory structure...\n\n";
+tput sgr0;
+
+source $(dirname $0)/../script-info.sh;
+
+for PAGE in controller preshowvideo projector streamHost
+do
+    mkdir $PATH_TO_SITE/$PAGE 2>/dev/null;
+done
+
+exit 0;
