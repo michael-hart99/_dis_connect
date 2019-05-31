3aa1ae0fdab5eadcc741e9cac6b570577bbb29c7
Michae Hart <hartmj99@cs.washington.edu>
2019-05-31 10:59:25 -0700

    Update start-server to not require sudo permissions

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+source $(dirname $0)/script_info.sh;
+
 info_msg "Starting server...";
 
-sudo ./node_modules/.bin/ts-node-dev --respawn --transpileOnly ./server/server.ts
+res="$(ts-node -T ./server/server.ts 2>&1)";
+
+if [ $? -ne 0 ]
+then
+    case $res in
+        *"permission denied"*)
+            info_msg "Permission denied when accessing HTTPS certificates"
+            info_msg "Starting server as root...";
+            sudo ./node_modules/.bin/ts-node -T ./server/server.ts
+            ;;
+        *"Unable to compile TypeScript"*)
+            ts-node ./server/server.ts;
+            exit 1
+            ;;
+        *)
+            err_msg "Unknown error during TypeScript transpilation:"
+            printf "$res\n"
+            exit 1
+            ;;
+    esac
+fi
